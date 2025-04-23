e8348829c80e33f7b6d2984dd72b8d1d3dcf5767
Jack-Peralta-Student <japecc2@student.oaisd.org>
2025-04-23 10:45:24 -0400

    Tower update

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -7,10 +7,28 @@ namespace TowerDefense
    public class Tower : MonoBehaviour
     {
         [SerializeField] private List<GameObject> enemiesInRange = new List<GameObject>();
+        private int damage = 1;
+        private float fireRate = 2f;
+        private bool firing = false;
+
+        IEnumerator DamageEnemyTarget()
+        {
+            firing = true;
+            while(enemiesInRange.Count > 0)
+            {
+                if (!enemiesInRange[0]) enemiesInRange.RemoveAt(0);
+                else Health.TryDamage(enemiesInRange[0], damage);
+
+                yield return new WaitForSeconds(fireRate);
+            }
+            firing = false;
+        }
 
         private void OnTriggerEnter(Collider other)
         {
            if (other.gameObject.CompareTag("Enemy")) enemiesInRange.Add(other.gameObject);
+
+           if (!firing) StartCoroutine(DamageEnemyTarget());
         }
 
         private void OnTriggerExit(Collider other)
